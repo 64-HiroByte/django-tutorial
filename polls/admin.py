0882f4ecbc12eeafb5038f12cf34fe6216bd0a78
64-HiroByte <higher.ground.rc@gmail.com>
2024-09-15 22:19:43 +0900

    adminフォームのカスタマイズ

--- polls/admin.py
+++ polls/admin.py
@@ -4,4 +4,8 @@ from .models import Question
 
 
 # Register your models here.
-admin.site.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ['pub_date', 'question_text']
+
+
+admin.site.register(Question, QuestionAdmin)
