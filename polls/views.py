019cc59200062d1481937af3c9c15558484b018c
64-HiroByte <higher.ground.rc@gmail.com>
2024-09-15 16:13:37 +0900

    views.pyのvote()を変更した

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,10 @@
 from django.http import HttpResponse
-# from django.http import Http404
+from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404
 from django.shortcuts import render
+from django.urls import reverse
 
-
+from .models import Choice
 from .models import Question
 
 # Create your views here.
@@ -26,4 +27,20 @@ def results(request, question_id):
 
     
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(
+            request,
+            'polls/detail.html',
+            {
+                'question': question,
+                'error_message': "You did't select a choice."
+            },
+        )
+    else:
+        selected_choice.vote += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question_id)))
+    
